4924cf3d2f8a78cf1d1f87fa8b72300e4830dd06
AmaraABC <amara-noah.gakou@efrei.net>
2025-02-19 17:32:44 +0100

    Ajout de lignes de code pour tester le lancement d'un serveur sur un port

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 
 // Liste des endpoints pour l'application
-app.get('/', (req, res) => { // Simulation / débogage (?)
+app.get('/', (req, res) => { // Afficher 'Hello World' en haut de la page
     res.send('Hello World !');
 });
-
-
 
 // Définition d'un port d'écoute
 const PORT = process.env.PORT || 3000;
